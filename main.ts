b56928eb2b106f401414343f5eab0d316a2b6d0b
Kuan (黃 冠龍) <151490037+HuangKuanLung@users.noreply.github.com>
2024-03-08 12:22:02 +0800

    Refactor main TypeScript module
     - Updated main.ts with optimizations and refactoring for better performance.
     - Added .gitignore to the project to manage untracked files.

--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@ import { Construct } from "constructs";
 import { EcsServiceStack } from "./ecs-service"
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { App, TerraformStack } from "cdktf";
-import { AWS_CONFIG_DEV } from "./localstack-config";
+import { AWS_CONFIG } from "./localstack-config";
 import { Instance } from "@cdktf/provider-aws/lib/instance";
 
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
-    new AwsProvider(this, 'AWS', AWS_CONFIG_DEV);
+    new AwsProvider(this, 'AWS', AWS_CONFIG);
     // new AwsProvider(this, 'AWS', AWS_CONFIG_DEV);
 
     new EcsServiceStack(this, 'my-ecs-service');  
